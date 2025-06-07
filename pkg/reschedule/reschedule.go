b14a8c2ba31a0038668f1679425f5c7821fb5d3a
BenMotts <ben.mottershead@couchbase.com>
2025-06-07 16:50:42 +0100

    Use new client implementation for interacting with resources. Clean up logic. Add improved comments

--- pkg/reschedule/reschedule.go
+++ pkg/reschedule/reschedule.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"slices"
 	"syscall"
 	"time"
 
@@ -17,16 +18,12 @@ import (
 	policyv1 "k8s.io/api/policy/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
 const (
-	RescheduleAnnotation         = "cao.couchbase.com/reschedule"
-	RescheduleHookFlagAnnotation = "reschedule.hook/added"
-	RescheduleTrue               = "true"
-	CouchbasePodLabelKey         = "app"
-	CouchbasePodLabelValue       = "couchbase"
+	CouchbasePodLabelKey   = "app"
+	CouchbasePodLabelValue = "couchbase"
+	TrackRescheduledPods   = true
 )
 
 func tlsConfig() *tls.Config {
@@ -51,20 +48,6 @@ func tlsConfig() *tls.Config {
 	return &tls.Config{
 		Certificates: []tls.Certificate{cert},
 	}
-}
-
-func getKubeClient() (kubernetes.Interface, error) {
-	kubeConfig, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	clientset, err := kubernetes.NewForConfig(kubeConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return clientset, nil
 }
 
 func Serve() {
@@ -116,9 +99,6 @@ func serveDefault(w http.ResponseWriter, r *http.Request) {
 }
 
 func serveEviction(w http.ResponseWriter, r *http.Request) {
-	slog.Info("Pod eviction request received")
-
-	// Read the POST body content
 	var body []byte
 	if r.Body != nil {
 		if data, err := io.ReadAll(r.Body); err == nil {
@@ -155,7 +135,7 @@ func serveEviction(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Initialise the Kubernetes client
-	client, err := getKubeClient()
+	client, err := NewClient()
 	if err != nil {
 		slog.Error("Failed to create Kubernetes client", "error", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -164,7 +144,6 @@ func serveEviction(w http.ResponseWriter, r *http.Request) {
 
 	// Handle the eviction request
 	response := handleEviction(eviction, client)
-
 	// Set the UID of the response to the UID of the request
 	response.UID = reviewRequest.Request.UID
 
@@ -184,64 +163,103 @@ func serveEviction(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-
 	if _, err := w.Write(resp); err != nil {
 		slog.Error("Failed to write admission review response", "error", err)
 	}
 }
 
-func handleEviction(eviction policyv1.Eviction, client kubernetes.Interface) *admissionv1.AdmissionResponse {
+func handleEviction(eviction policyv1.Eviction, client Client) *admissionv1.AdmissionResponse {
 	slog.Info("Handling eviction request",
 		"pod", eviction.Name,
 		"namespace", eviction.Namespace)
 
-	pod, err := client.CoreV1().Pods(eviction.Namespace).Get(context.TODO(), eviction.Name, metav1.GetOptions{})
-
-	// TODO It's possible that the pod has been rescheduled, therefore if the pod does not exist, we should allow drain to continue
-	// IF an inplaceupgrade has taken place, we need to consider that the pod may have the same name but already rescheduled
+	pod, err := client.GetPod(eviction.Namespace, eviction.Name)
+	// If the pod doesn't exist, we can assume that it has already been recreated with a different name since the drain request
 	if err != nil {
 		if k8serrors.IsNotFound(err) {
 			slog.Info("Pod has been rescheduled", "pod", eviction.Name, "namespace", eviction.Namespace)
 			return allowEviction()
 		}
+
 		slog.Error("Failed to get pod", "error", err)
 		return denyEviction(http.StatusInternalServerError, metav1.StatusReasonInternalError, "Failed to get pod")
 	}
 
 	if pod.Labels[CouchbasePodLabelKey] != CouchbasePodLabelValue {
-		slog.Info("Pod is not a couchbase pod", "pod", pod.Name, "namespace", pod.Namespace)
+		slog.Info("Pod is not a couchbase node", "pod", pod.Name, "namespace", pod.Namespace)
 		return allowEviction()
 	}
 
+	// If the pod has already been marked for rescheduling, we can exit here but deny the eviction to keep the drain command
+	// in a loop until the pod no longer exists.
 	if reschedule, exists := pod.GetAnnotations()[RescheduleAnnotation]; exists && reschedule == RescheduleTrue {
 		slog.Info("Couchbase pod awaiting reschedule", "pod", pod.Name, "namespace", pod.Namespace)
 		return denyEviction(http.StatusTooManyRequests, metav1.StatusReasonTooManyRequests, "Couchbase pod awaiting reschedule")
-	} else if !exists {
-		// TODO We need to add a second annotation here to mark that we have added a reschedule annotation.
-		slog.Info("Adding annotations to pod", "pod", pod.Name, "namespace", pod.Namespace)
-		// If both annotations are present, we can quit
-		// If only the mark annotation is present, we can assume the pod has been rescheduled and skip/return to break the kubectl for loop to allow the eviction
-		annotations := pod.GetAnnotations()
-		if annotations == nil {
-			annotations = make(map[string]string)
-		}
-
-		annotations[RescheduleAnnotation] = RescheduleTrue
-		pod.SetAnnotations(annotations)
-
-		_, err = client.CoreV1().Pods(eviction.Namespace).Update(context.TODO(), pod, metav1.UpdateOptions{})
-		if err != nil {
-			slog.Error("Failed to add reschedule annotation to pod", "error", err)
-			return denyEviction(http.StatusInternalServerError, metav1.StatusReasonInternalError, "Failed to add reschedule annotation to pod")
-		}
-
-		// TODO If the pod has already been evicted, we need to exit the drain request loop as the pod no longer exists but kubectl will continue to try to evict it as it isn't refreshed
-		// TODO Potentially, we only need to add the reschedule annotation if the pod is on a cordoned node?
-		return denyEviction(http.StatusTooManyRequests, metav1.StatusReasonTooManyRequests, "Reschedule annotation added to pod")
 	}
 
-	// If we get to this point, we can assume all is fine and dandy and allow the eviction request to continue
-	return allowEviction()
+	// If the pod has not been marked for rescheduling, it's possible it has already been rescheduled with the same name.
+	// We can track pods that have been already been marked for rescheduling using an annotation on the CouchbaseCluster
+	// resource the pod (couchbase node) belongs to. We will only add to this list if the cluster is using InPlaceUpgrade, but
+	// will always attempt to remove the pod from the list.
+	// Skip this step by setting the TrackRescheduledPods flag to false.
+	if TrackRescheduledPods {
+		clusterName := pod.Labels[CouchbaseClusterLabelKey]
+
+		response := handlePreviouslyRescheduledPodsList(pod.Name, clusterName, eviction.Namespace, client, true)
+		if response != nil {
+			return response
+		}
+	}
+
+	// At this point, we can assume the pod has not already been rescheduled and should therefore be marked for rescheduling.
+	slog.Info("Adding reschedule annotation to pod", "pod", pod.Name, "namespace", pod.Namespace)
+	err = client.ReschedulePod(pod)
+	if err != nil {
+		slog.Error("Failed to add reschedule annotation to pod", "error", err)
+		return denyEviction(http.StatusInternalServerError, metav1.StatusReasonInternalError, "Failed to add reschedule annotation to pod")
+	}
+
+	// By denying the eviction with StatusReasonTooManyRequests, the drain command will continue attempting to evict
+	// the pod every 5 seconds until it has been rescheduled correctly.
+	return denyEviction(http.StatusTooManyRequests, metav1.StatusReasonTooManyRequests, "Reschedule annotation added to pod")
+}
+
+// handlePreviouslyRescheduledPodsList handles situations where a pod may have been rescheduled with the same name. This method will
+// check an annotation on the couchbase cluster resource for a list of pods that have been rescheduled.
+// When calling this method, we know that the pod does not have the reschedule annotation. Therefore, if it is present in the list,
+// we can assume that it has already been rescheduled and allow the eviction to proceed. If it is not present in the list
+// and the cluster is using InPlaceUpgrade, the pod will be added to the list, in preparation for the next eviction attempt.
+func handlePreviouslyRescheduledPodsList(pod, cluster, namespace string, client Client, shouldUpdateList bool) *admissionv1.AdmissionResponse {
+	clusterInfo, err := client.GetClusterInfo(cluster, namespace)
+	if err != nil {
+		slog.Error("error fetching required cluster info", "error", err)
+		return denyEviction(http.StatusInternalServerError, metav1.StatusReasonInternalError, "Failed to get required cluster info")
+	}
+
+	if slices.Contains(clusterInfo.rescheduleHookPodsList, pod) {
+		slog.Info("Pod has already been rescheduled, removing from tracking list if needed", "pod", pod, "namespace", namespace)
+		err = client.PatchRescheduleHookPodsList(cluster, namespace, slices.DeleteFunc(clusterInfo.rescheduleHookPodsList, func(s string) bool {
+			return s == pod
+		}))
+
+		if err != nil {
+			slog.Error("Failed to remove pod from tracking list", "error", err)
+			return denyEviction(http.StatusInternalServerError, metav1.StatusReasonInternalError, "Failed to remove pod from tracking list")
+		}
+
+		return allowEviction()
+	}
+
+	if shouldUpdateList {
+		err = client.PatchRescheduleHookPodsList(cluster, namespace, append(clusterInfo.rescheduleHookPodsList, pod))
+
+		if err != nil {
+			slog.Error("Failed to update tracking list", "error", err)
+			return denyEviction(http.StatusInternalServerError, metav1.StatusReasonInternalError, "Failed to update rescheduled pods tracking list")
+		}
+	}
+
+	return nil
 }
 
 func denyEviction(code int32, reason metav1.StatusReason, message string) *admissionv1.AdmissionResponse {
